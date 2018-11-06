aba1c504f8cef60cf61955c127e0490526e26051
Neto Concon <concon.neto2@gmail.com>
2018-11-06 19:33:29 -0200

    add pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+  get 'pages/service'
+  get 'pages/clients'
+  get 'pages/blog'
+  get 'pages/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
